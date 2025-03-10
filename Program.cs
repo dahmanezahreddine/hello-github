d0d4859eec94184ceadaa35c4884c74829d66857
zahreddine <dahmanezahreddine@gmail.com>
2025-03-10 18:22:54 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace main.cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, GitHub! Je mappelle Zahreddine Dahmane");
+        }
+    }
+}
